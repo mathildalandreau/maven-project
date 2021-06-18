4c3bb9443b367d810969f62e5487d4d21ef3ffa6
mathildalandreau <73292823+mathildalandreau@users.noreply.github.com>
2021-06-18 11:39:38 +0200

    updateGreter
    commentaire constructeur + méthode

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,11 +3,11 @@ package com.example;
 
 
 public class Greeter {
-
+/*constructeur vide*/
   public Greeter() {
 
   }
-
+/* Méthode qui renvoit "Hello babacar" */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
