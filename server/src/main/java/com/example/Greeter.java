26de626ee9ef3ae756c3658636e4c9457c81400d
mathildalandreau <73292823+mathildalandreau@users.noreply.github.com>
2021-06-18 11:41:26 +0200

    update greeter
    @param

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,7 @@ public class Greeter {
   public Greeter() {
 
   }
-/* Méthode qui renvoit "Hello babacar" */
+/** Méthode qui renvoit "Hello babacar" @param someone une personne */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
