38a64bc420c390ffa0f1087c7e3869dd7439ee91
mathildalandreau <73292823+mathildalandreau@users.noreply.github.com>
2021-06-18 11:50:39 +0200

    update Greeter
    ajout "."

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,23 +1,24 @@
+package-info.java file;
 package com.example;
-/**
+/**.
  * Classe exemple pour javadoc
  * @author MAthilda
- * @version 1.0 
+ * @version 1.0
  * @since 18/0682021
  */
 public class Greeter {
-/**
+/**.
 * Creates an empty Greeter
 */
   public Greeter() {
 
   }
-/** 
-*Methode qui renvoit "Hello babacar" 
-*@param someone une personne 
+/**.
+*Methode qui renvoit "Hello babacar"
+*@param someone une personne
 */
   public final String greet(final String someone) {
-    /**
+    /**.
     *@return
     */
     return String.format("Hello babacar, %s!", someone);
